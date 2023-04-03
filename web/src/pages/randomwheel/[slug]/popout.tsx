c69e10583ce9f8dfea8b098e0e2cf985e4ae481f
Xeeija <19950592+xeeija@users.noreply.github.com>
2023-04-04 01:12:27 +0200

    update randomwheel popout page to use useRandomWheel

--- web/src/pages/randomwheel/[slug]/popout.tsx
+++ web/src/pages/randomwheel/[slug]/popout.tsx
@@ -1,11 +1,10 @@
 import { Box, useTheme } from "@mui/material";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
 import { getTitle, LayoutNextPage } from "../../../components/layout";
 import { Wheel } from "../../../components/randomWheel";
-import { useRandomWheelBySlugEntriesQuery, useRandomWheelBySlugQuery } from "../../../generated/graphql";
+import { useRandomWheel } from "../../../hooks";
+import NotFoundPage from "../../404";
 
 const RandomWheelPopoutPage: LayoutNextPage = () => {
 
@@ -14,85 +13,21 @@ const RandomWheelPopoutPage: LayoutNextPage = () => {
 
   const theme = useTheme()
 
-  const [{ data, fetching }, fetchWheel] = useRandomWheelBySlugQuery({
-    variables: {
-      slug: typeof slug === "string" ? slug : slug?.[0] ?? ""
-    }
+  const [
+    { wheel, entries, fetching },
+  ] = useRandomWheel(slug ?? "", {
+    details: true,
+    entries: true,
   })
-
-  const [{ data: entriesData }, fetchEntries] = useRandomWheelBySlugEntriesQuery({
-    variables: {
-      slug: typeof slug === "string" ? slug : slug?.[0] ?? ""
-    }
-  })
-
-  const [wheelRotation, setWheelRotation] = useState(0)
-  const [spinning, setSpinning] = useState(false)
-
-  const wheel = data?.randomWheelBySlug
-  const entries = entriesData?.randomWheelBySlug?.entries
 
   const title = wheel?.name || `Wheel #${slug}`
 
-  // socket
-  useEffect(() => {
-    if (!wheel?.id) {
-      // console.log("no wheel")
-      return
-    }
+  if (!wheel) {
+    // TODO: Proper error pages
+    return <NotFoundPage />
+  }
 
-    const socket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER_URL ?? process.env.NEXT_PUBLIC_SERVER_URL ?? "http://localhost:4000", {
-      path: process.env.NEXT_PUBLIC_SOCKET_SERVER_PATH ?? "/socket",
-    })
-
-    socket.on("connect", () => {
-      // console.log("connect")
-      socket.emit("wheel:join", wheel.id)
-      // console.log(`join ${wheel.id.substring(0, 6)}`)
-    })
-
-    socket.on("wheel:spin", ({ rotation }) => {
-      // console.log("wheel:spin", { rotation, winner, entry })
-
-      const revolutions = ~~(Math.random() * 2 + (wheel.spinDuration / 1000) - 1)
-      // console.log(revolutions)
-
-      setSpinning(true)
-      setWheelRotation(rotation + (360 * revolutions))
-
-      setTimeout(() => {
-        setSpinning(false)
-        setWheelRotation(rotation)
-      }, wheel.spinDuration + 500 + 10)
-
-    })
-
-    socket.on("wheel:entries", () => {
-      // console.log("wheel:entries")
-
-      // TODO: Refactor to update the "local" entries with entry from socket?
-      // Depending on type, add/delete/clear
-      fetchEntries({
-        requestPolicy: "cache-and-network",
-      })
-    })
-
-    socket.on("wheel:update", () => {
-      // console.log("wheel")
-
-      fetchWheel({
-        requestPolicy: "cache-and-network",
-      })
-    })
-
-    return () => {
-      socket.off("wheel:spin")
-      socket.disconnect()
-      // console.log(`disconnect ${wheel.id.substring(0, 6)}`)
-    }
-  }, [wheel?.id, wheel?.spinDuration, fetchEntries, fetchWheel])
-
-  if (fetching || !slug) {
+  if (fetching.wheel || !slug) {
     return <Head>
       <title>{getTitle()}</title>
     </Head>
@@ -110,10 +45,10 @@ const RandomWheelPopoutPage: LayoutNextPage = () => {
         visibility: "hidden",
         transition: theme.transitions.create(["opacity", "visibility"], {
           duration: 500,
-          delay: spinning ? 0 : wheel?.fadeDuration ?? 5000,
+          delay: wheel.spinning ? 0 : wheel?.fadeDuration ?? 5000,
           easing: theme.transitions.easing.easeOut
         }),
-        ...(spinning && {
+        ...(wheel.spinning && {
           opacity: 1,
           visibility: "visible"
         }),
@@ -121,8 +56,8 @@ const RandomWheelPopoutPage: LayoutNextPage = () => {
         <Wheel
           diameter={680}
           entries={entries}
-          rotation={(wheelRotation || wheel?.rotation) ?? 0}
-          spinning={spinning}
+          rotation={wheel.rotation}
+          spinning={wheel.spinning}
           spinDuration={wheel?.spinDuration ?? 0}
         />
       </Box>
