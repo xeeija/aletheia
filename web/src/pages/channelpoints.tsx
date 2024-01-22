e5fb66c80c5fef7fd4f985c7ea80ec54f6b03332
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-01-22 10:53:46 +0100

    update channelpoints page

--- web/src/pages/channelpoints.tsx
+++ web/src/pages/channelpoints.tsx
@@ -1,61 +1,159 @@
-import { Button, SvgIcon, Typography } from "@mui/material"
+import { Box, Button, IconButton, SvgIcon, Tab, Tabs, Tooltip } from "@mui/material"
 import { FormikProps } from "formik"
 import { useRef, useState } from "react"
+import { HiDotsVertical } from "react-icons/hi"
 import { TiPlus } from "react-icons/ti"
-import { FormDialog } from "../components"
+import { FormDialog, NoData } from "../components"
 import { LayoutNextPage, defaultLayout } from "../components/layout"
 import { CreateChannelRewardForm } from "../components/twitch"
 import { useChannelRewards } from "../hooks"
 
 export const ChannelPointsPage: LayoutNextPage = () => {
 
-  const [createDialogOpen, setCreateDialogOpen] = useState(false)
+  const [createRewardDialogOpen, setCreateRewardDialogOpen] = useState(false)
+  const [createGroupDialogOpen, setCreateGroupDialogOpen] = useState(false)
+  const [tab, setTab] = useState(0)
   const formRef = useRef<FormikProps<any>>(null)
   const actionsRef = useRef(null)
 
-  const { channelRewards } = useChannelRewards()
+  const { channelRewards, fetching: fetchingRewards } = useChannelRewards()
+
+  const channelRewardsEmpty = (channelRewards?.length ?? 0) === 0
 
   return (
     <>
-      <Typography variant="h4" sx={{ mb: 1 }}>Rewards</Typography>
+      <Box sx={{
+        display: "flex",
+        justifyContent: "space-between"
+      }}>
+        <Tabs
+          value={tab}
+          onChange={(_, value) => setTab(value)}
+        >
+          {/* itemType prop for new variant, because there is no variant prop */}
+          <Tab label="Rewards" itemType="capitalize" />
+          <Tab label="Reward Groups" itemType="capitalize" />
+        </Tabs>
 
-      {/* <Link href="/channelpoints/create" passHref> */}
-      <Button
-        color="success"
-        variant="contained"
-        startIcon={<SvgIcon component={TiPlus} inheritViewBox />}
-        onClick={() => setCreateDialogOpen(true)}
-      >
-        Create Reward
-      </Button>
-      {/* </Link> */}
+        {tab === 0 && (
+          <Box>
+            <Button variant="outlined" color="success"
+              endIcon={<SvgIcon component={TiPlus} viewBox="0 1 24 24" />}
+              onClick={() => setCreateRewardDialogOpen(true)}
+            >
+              Create
+            </Button>
 
-      {channelRewards?.map((reward) => (
-        <pre key={reward.id}>
-          {reward.title}
-        </pre>
-      ))}
+            <Tooltip placement="bottom-end" title="More options">
+              <IconButton color="secondary" sx={{ ml: 1 }} disabled>
+                <HiDotsVertical />
+              </IconButton>
+            </Tooltip>
 
-      <FormDialog
-        keepMounted
-        maxWidth="sm"
-        title={"Create Channel Reward"}
-        open={createDialogOpen}
-        onClose={() => {
-          setCreateDialogOpen(false)
+          </Box>
+        )}
+        {tab === 1 && (
+          <Box>
+            <Button variant="outlined" color="success"
+              endIcon={<SvgIcon component={TiPlus} viewBox="0 1 24 24" />}
+              onClick={() => setCreateGroupDialogOpen(true)}
+            >
+              Create
+            </Button>
 
-          if (formRef.current) {
-            setTimeout(() => formRef.current?.resetForm(), 350)
-          }
-        }}
-        actionsRef={actionsRef}
-      >
-        <CreateChannelRewardForm actionsRef={actionsRef} formRef={formRef} />
-      </FormDialog>
+            <Tooltip placement="bottom-end" title="More options">
+              <IconButton color="secondary" sx={{ ml: 1 }} disabled>
+                <HiDotsVertical />
+              </IconButton>
+            </Tooltip>
+
+          </Box>
+        )}
+      </Box>
+
+      <Box sx={{ mt: 2 }}>
+        {tab === 0 && (
+          <Box>
+            {/* <Link href="/channelpoints/create" passHref> 
+              <Button
+                color="success"
+                variant="contained"
+                startIcon={<SvgIcon component={TiPlus} inheritViewBox />}
+                onClick={() => setCreateRewardDialogOpen(true)}
+              >
+                Create Reward
+              </Button>
+            </Link> */}
+
+            {channelRewards?.map((reward) => (
+              <pre key={reward.id}>
+                {reward.title}
+              </pre>
+            ))}
+
+            {channelRewardsEmpty && !fetchingRewards && (
+              <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <NoData>
+                  {"You don't have any channel point rewards yet."}
+                </NoData>
+
+                <Box>
+                  <Button variant="contained" color="success"
+                    endIcon={<SvgIcon component={TiPlus} viewBox="0 1 24 24" />}
+                    onClick={() => setCreateRewardDialogOpen(true)}
+                  >
+                    Create reward
+                  </Button>
+                </Box>
+              </Box>
+            )}
+
+            <FormDialog
+              keepMounted
+              maxWidth="sm"
+              title={"Create Channel Reward"}
+              open={createRewardDialogOpen}
+              onClose={() => {
+                setCreateRewardDialogOpen(false)
+
+                if (formRef.current) {
+                  setTimeout(() => formRef.current?.resetForm(), 350)
+                }
+              }}
+              actionsRef={actionsRef}
+            >
+              <CreateChannelRewardForm actionsRef={actionsRef} formRef={formRef} />
+            </FormDialog>
+          </Box>
+        )}
+
+        {tab === 1 && (
+          <Box>
+
+            {channelRewardsEmpty && !fetchingRewards && (
+              <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <NoData>
+                  {"You don't have any rewards groups yet."}
+                </NoData>
+
+                <Box>
+                  <Button variant="contained" color="success"
+                    endIcon={<SvgIcon component={TiPlus} viewBox="0 1 24 24" />}
+                    onClick={() => setCreateGroupDialogOpen(true)}
+                  >
+                    Create group
+                  </Button>
+                </Box>
+              </Box>
+            )}
+
+          </Box>
+        )}
+      </Box>
     </>
   )
 }
 
-ChannelPointsPage.getLayout = defaultLayout({ title: "Channel Points", navTitle: "Channel Points" })
+ChannelPointsPage.getLayout = defaultLayout({ title: "Channel Points", navTitle: "Channel Points", fullWidth: false })
 
 export default ChannelPointsPage
