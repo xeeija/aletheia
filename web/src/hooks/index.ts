af91c91ffda78c0413eee1aea23428c6609c56f5
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-03-05 11:25:15 +0100

    update web components structure

--- web/src/hooks/index.ts
+++ web/src/hooks/index.ts
@@ -1,5 +1,7 @@
-export * from "./randomwheel/useRandomWheel"
-export * from "./twitch/useChannelRewards"
-export * from "./twitch/useEventSubcriptions"
 export * from "./useAuth"
 export * from "./useUsernameValidator"
+
+export * from "./randomwheel/useRandomWheel"
+
+export * from "./twitch/useChannelRewards"
+export * from "./twitch/useEventSubcriptions"
