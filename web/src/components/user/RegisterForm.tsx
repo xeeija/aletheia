8a6366293ffb493c862786f249a5731aed8f463e
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-04-23 20:21:14 +0200

    update login and register form

--- web/src/components/user/RegisterForm.tsx
+++ web/src/components/user/RegisterForm.tsx
@@ -1,12 +1,13 @@
 import { InputField, LinkText, LoadingButton, PasswordField } from "@/components"
 import { useRegisterMutation } from "@/generated/graphql"
-import { useUsernameValidator } from "@/hooks"
+import { useAlert, useUsernameValidator } from "@/hooks"
 import { passwordStrength } from "@/utils/password"
-import { Alert, Collapse, SvgIcon, Typography } from "@mui/material"
+import { SvgIcon, Typography } from "@mui/material"
 import { Form, Formik } from "formik"
 import { useRouter } from "next/router"
-import { FC, useState } from "react"
-import { TiArrowRight, TiWarning } from "react-icons/ti"
+import { FC, useMemo } from "react"
+import { TiArrowRight } from "react-icons/ti"
+import { object, ref, string } from "yup"
 
 interface Props {}
 
@@ -14,11 +15,33 @@ export const RegisterForm: FC<Props> = () => {
   const [, register] = useRegisterMutation()
   const router = useRouter()
 
+  const { showSuccess, showError } = useAlert()
+
   // TODO: One useValidator hook with all (or almost/most common) validators?
   const [validateUsername] = useUsernameValidator()
+  const passwordStrengthFn = useMemo(() => passwordStrength, [])
 
-  const [generalError, setGeneralError] = useState<string | null>(null)
-  const [showError, setShowError] = useState(false)
+  const passwordHelperText = "Min. length 8, must contain a-z, A-Z, 0-9 and special letters"
+
+  const validationSchema = object().shape({
+    username: string().required("Required"),
+    // .min(4, "Must have at least 4 characters")
+    // .max(32, "Can only have 32 characters"),
+    // .test({
+    //   test: async (value) => (await validateUsername(value)) === undefined,
+    //   // message: "$resolved",
+    // }),
+    displayname: string(),
+    password: string()
+      .required("Required")
+      .test({
+        test: (value) => passwordStrengthFn(value) > 40,
+        message: passwordHelperText,
+      }),
+    confirmPassword: string()
+      .required("Required")
+      .oneOf([ref("password").resolve()], "Must match password"),
+  })
 
   return (
     <Formik
@@ -29,39 +52,28 @@ export const RegisterForm: FC<Props> = () => {
         confirmPassword: "",
       }}
       validateOnChange={false}
-      validateOnBlur={false}
-      validate={({ password, confirmPassword }) => {
-        const errors: Record<string, string> = {}
-
-        if (password !== confirmPassword) errors.confirmPassword = "Passwords must match"
-
-        return errors
-      }}
+      // validateOnBlur={false}
+      validationSchema={validationSchema}
       onSubmit={async (values, { setFieldError, setFieldTouched }) => {
-        // Reset error message (so same error shows again)
-        setShowError(false)
-
-        const { displayname, ...rest } = values
-
         const response = await register({
-          displayname: displayname || null, // pass null if empty
-          ...rest,
+          ...values,
+          displayname: values.displayname || null, // pass null if empty
         })
 
         // Unexpected error
         if (response.error) {
-          let errorMsg: string
-          if (response.error.networkError) errorMsg = "Could not connect to login server."
-          else if (response.error.graphQLErrors) errorMsg = "The login server is currently unavailable."
-          else errorMsg = "Unknown error, please try again later."
+          if (response.error.networkError) {
+            showError("Could not connect to login server.")
+          } else if (response.error.graphQLErrors) {
+            showError(response.error.graphQLErrors.join("\\n"))
+          } else {
+            showError("Unknown error, please try again later.")
+          }
 
           // Reset password
           values.password = ""
           values.confirmPassword = ""
 
-          // Show error alert
-          setGeneralError(errorMsg)
-          setShowError(true)
           return
         }
 
@@ -80,26 +92,15 @@ export const RegisterForm: FC<Props> = () => {
           return
         }
 
+        showSuccess("Registered successfully")
+
         // TODO: Redirect to "welcome page"?
         // Redirect to home page
         await router.push("/")
       }}
     >
-      {({ isSubmitting, values }) => (
+      {({ isSubmitting, values, isValid, submitCount }) => (
         <Form>
-          {/* Error Alert */}
-          <Collapse in={showError} onExited={() => setGeneralError("")}>
-            <Alert
-              severity="error"
-              variant="filled"
-              icon={<TiWarning />}
-              onClose={() => setShowError(false)}
-              sx={{ my: 1 }}
-            >
-              {generalError}
-            </Alert>
-          </Collapse>
-
           <InputField
             name="username"
             label="Username"
@@ -107,6 +108,8 @@ export const RegisterForm: FC<Props> = () => {
             fullWidth
             required
             icon
+            maxLength={32}
+            showMaxLength={24}
             validate={validateUsername}
           />
 
@@ -122,19 +125,8 @@ export const RegisterForm: FC<Props> = () => {
             required
             icon
             // TODO: passworStrength is called in every render, maybe useMemo?
-            strength={passwordStrength(values.password)}
-            helperText="Min. length 8, must contain a-z, A-Z, 0-9 and special letters"
-            validate={(value: string) => {
-              if (!value) {
-                return "Please enter a password"
-              }
-
-              if (passwordStrength(value) <= 40) {
-                return "Min. length 8, must contain a-z, A-Z, 0-9 and special letters"
-              }
-
-              return undefined
-            }}
+            strength={passwordStrengthFn(values.password)}
+            helperText={passwordHelperText}
           />
 
           <InputField
@@ -144,14 +136,7 @@ export const RegisterForm: FC<Props> = () => {
             margin="normal"
             fullWidth
             required
-            validate={(value) => {
-              if (!value) {
-                return "Please confirm your password"
-              }
-              // TODO: Fix (no context where the hook is initialized)
-              // return matchField("password", value, "Passwords have to match")
-              return undefined
-            }}
+            icon
           />
 
           <LoadingButton
@@ -159,6 +144,7 @@ export const RegisterForm: FC<Props> = () => {
             variant="contained"
             fullWidth
             loading={isSubmitting}
+            disabled={submitCount > 0 && !isValid}
             position="end"
             endIcon={<SvgIcon component={TiArrowRight} />}
             sx={{ mt: 2 }}
