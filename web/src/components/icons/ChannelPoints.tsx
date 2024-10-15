a281029cd7f1af3c85caa984d4fcc3c55fb527b5
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-10-15 16:20:00 +0200

    refactor createSvgIcon to SvgIcon to wrk with server components

--- web/src/components/icons/ChannelPoints.tsx
+++ web/src/components/icons/ChannelPoints.tsx
@@ -1,15 +1,29 @@
-import { createSvgIcon } from "@mui/material"
+import { SvgIcon, SvgIconProps } from "@mui/material"
+import { FC } from "react"
 
-export const ChannelPoints = createSvgIcon(
-  // <svg viewBox="0 0 20 20">
-  <>
-    <path d="M10 6a4 4 0 0 1 4 4h-2a2 2 0 0 0-2-2V6z"></path>
-    <path
-      d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0zm-2 0a6 6 0 1 1-12 0 6 6 0 0 1 12 0z"
-      clipRule="evenodd"
-      fillRule="evenodd"
-    ></path>
-  </>,
-  // </svg>,
-  "ChannelPoints"
-)
+export const ChannelPoints: FC<SvgIconProps> = (props) => {
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 6a4 4 0 0 1 4 4h-2a2 2 0 0 0-2-2V6z" />
+      <path
+        d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0zm-2 0a6 6 0 1 1-12 0 6 6 0 0 1 12 0z"
+        clipRule="evenodd"
+        fillRule="evenodd"
+      />
+    </SvgIcon>
+  )
+}
+
+// export const ChannelPoints2 = createSvgIcon(
+//   // <svg viewBox="0 0 20 20">
+//   <>
+//     <path d="M10 6a4 4 0 0 1 4 4h-2a2 2 0 0 0-2-2V6z"></path>
+//     <path
+//       d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0zm-2 0a6 6 0 1 1-12 0 6 6 0 0 1 12 0z"
+//       clipRule="evenodd"
+//       fillRule="evenodd"
+//     ></path>
+//   </>,
+//   // </svg>,
+//   "ChannelPoints"
+// )
