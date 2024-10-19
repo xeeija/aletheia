b3ed321bea5221c81279b11568d33eaec9c0f84f
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-10-19 03:05:08 +0200

    add explicit re-exports to fix client components (unsupported server component)

--- web/src/components/icons/index.ts
+++ web/src/components/icons/index.ts
@@ -1,3 +1,7 @@
-export * from "./ChannelPoints"
-export * from "./LinkAdd"
-export * from "./SendMessage"
+// export * from "./ChannelPoints"
+// export * from "./LinkAdd"
+// export * from "./SendMessage"
+
+export { ChannelPoints } from "./ChannelPoints"
+export { LinkAdd } from "./LinkAdd"
+export { SendMessage } from "./SendMessage"
