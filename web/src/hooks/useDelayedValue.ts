ff31ff5319f688e32e17a3a985d8eea482957416
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-11-09 15:08:05 +0100

    add react compiler eslint plugin, fix new lint rules

--- web/src/hooks/useDelayedValue.ts
+++ web/src/hooks/useDelayedValue.ts
@@ -4,11 +4,6 @@ import { useEffect } from "react"
 export const useDelayedValue = <T>(value: T, delayMs: number, immediatePredicate?: (value: T) => boolean) => {
   const [delayedValue, updateValue] = useDelayedState(value, delayMs)
 
-  // console.log("delayedValue call", value)
-  // useEffect(() => {
-  //   console.log("delayedValue initial")
-  // }, [])
-
   useEffect(() => {
     // if (previous === delayedValue) {
     //   return
@@ -19,26 +14,7 @@ export const useDelayedValue = <T>(value: T, delayMs: number, immediatePredicate
     // console.log("delayedValue update", { value, immediate })
 
     updateValue(value, immediate ? 0 : delayMs)
-
-    // updateValue(
-    //   (previousValue) => {
-    //     console.log("delayedValue set", { value, immediate, previousValue })
-    //     // console.log("delayedValue set", { delayedValue, previousValue, immediateUpdate: immediate })
-    //     // setPrevious(previousValue)
-    //     return value
-    //   },
-    //   immediate ? 0 : delayMs
-    // )
-
-    // return () => {
-    //   console.log("clear")
-    //   clear()
-    // }
-    // return () => {
-    //   console.log("delayedValue cleanup", { delayedValue, immediateUpdate })
-    // }
   }, [value, delayMs, updateValue, immediatePredicate])
-  // }, [value, delayedValue, delayMs, updateValue, immediatePredicate, previous])
 
   return delayedValue
 }
