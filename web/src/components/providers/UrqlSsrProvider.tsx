401031b532c9afc335f1d3e019f9a38b7b8ae509
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-11-17 00:41:25 +0100

    update to graphcache for normalized caching in urql

--- web/src/components/providers/UrqlSsrProvider.tsx
+++ web/src/components/providers/UrqlSsrProvider.tsx
@@ -1,26 +1,85 @@
 "use client"
 
+import { LoginMutation, MeDocument, MyRandomWheelsDocument } from "@/generated/graphql"
 import { devtoolsExchange } from "@urql/devtools"
-import { cacheExchange, createClient, fetchExchange, ssrExchange, UrqlProvider } from "@urql/next"
+import { cacheExchange } from "@urql/exchange-graphcache"
+import { createClient, fetchExchange, ssrExchange, UrqlProvider } from "@urql/next"
 import { FC, ReactNode, useMemo } from "react"
 
 interface Props {
   children: ReactNode
 }
 
-const isClient = typeof window !== "undefined"
-export const ssrCache = ssrExchange({ isClient })
+// const isClient = typeof window !== "undefined"
+// export const ssrCache = ssrExchange({ isClient })
 
 export const UrqlSsrProvider: FC<Props> = ({ children }) => {
   const [client, ssr] = useMemo(() => {
     const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL ?? ""
-    // const isClient = typeof window !== "undefined"
 
-    // const ssr = ssrExchange({ isClient })
+    const isClient = typeof window !== "undefined"
+    const ssrCache = ssrExchange({ isClient })
+
+    const graphCache = cacheExchange({
+      keys: {
+        RandomWheelCount: () => null,
+      },
+      updates: {
+        Mutation: {
+          login: (result: LoginMutation, _args, cache) => {
+            // console.log("login", { result, _args, _info })
+
+            cache.updateQuery({ query: MeDocument }, (data) => ({
+              ...data,
+              me: result.login.user,
+            }))
+
+            // const queryFields = cache.inspectFields("Query")
+            // console.log({ queryFields })
+
+            // const cachedWheel = cache.resolve("Query", "myRandomWheels", { type: "my" })
+            // const cachedMe = cache.resolve("Query", "me")
+
+            // console.log({ cachedWheel, cachedMe })
+
+            const key = "Query"
+            cache
+              .inspectFields(key)
+              .filter((field) => field.fieldName === "myRandomWheels")
+              .forEach((field) => {
+                cache.invalidate(key, field.fieldKey)
+                // or alternatively:
+                cache.invalidate(key, field.fieldName, field.arguments)
+              })
+          },
+          logout: (_result, _args, cache) => {
+            cache.updateQuery({ query: MeDocument }, (data) => ({
+              ...data,
+              me: null,
+            }))
+
+            const key = "Query"
+            cache
+              .inspectFields(key)
+              .filter((field) => field.fieldName === "myRandomWheels")
+              .forEach((field) => {
+                cache.updateQuery(
+                  { query: MyRandomWheelsDocument, variables: field.arguments ?? undefined },
+                  (data) => ({ ...data, myRandomWheels: [] })
+                )
+
+                //     // cache.invalidate(key, field.fieldKey)
+                //     // or alternatively:
+                //     // cache.invalidate(key, field.fieldName, field.arguments)
+              })
+          },
+        },
+      },
+    })
 
     const client = createClient({
       url: `${serverUrl}/api/graphql`,
-      exchanges: [devtoolsExchange, cacheExchange, ssrCache, fetchExchange],
+      exchanges: [devtoolsExchange, graphCache, ssrCache, fetchExchange],
       // exchanges: [devtoolsExchange, cacheExchange, ssr, fetchExchange],
       suspense: true,
       fetchOptions: {
