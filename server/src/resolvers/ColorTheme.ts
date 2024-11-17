3f9561964dd3dae48941310ded4465faa6d9c433
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-11-17 15:24:34 +0100

    update union type checks in resolvers (myRandomWheels, colorThemes)

--- server/src/resolvers/ColorTheme.ts
+++ server/src/resolvers/ColorTheme.ts
@@ -2,24 +2,30 @@ import { ColorTheme } from "@/resolvers/index.js"
 import type { GraphqlContext } from "@/types.js"
 import { Arg, Ctx, Query, Resolver } from "type-graphql"
 
+const AllColorThemeTypes = ["all", "my", "global"] as const
+type ColorThemeType = (typeof AllColorThemeTypes)[number]
+
+const isColorThemeType = (value: string): value is ColorThemeType => {
+  return AllColorThemeTypes.includes(value as ColorThemeType)
+}
+
 @Resolver(() => ColorTheme)
 export class ColorThemeResolver {
   @Query(() => [ColorTheme])
   async colorThemes(
     @Ctx() { req, prisma }: GraphqlContext,
     // @Info() info: GraphQLResolveInfo,
-    @Arg("type", { defaultValue: "all" }) type: "all" | "my" | "global"
+    @Arg("type", { defaultValue: "all" }) type: ColorThemeType
   ) {
-    if (!["all", "my", "global"].includes(type)) {
-      // TODO: unsupported type, maybe use enum instead and throw proper Graphql Error
-      return []
+    if (!isColorThemeType(type)) {
+      throw new Error(`Invalid type. Must be one of: ${AllColorThemeTypes.join(", ")}`)
     }
 
     const themes = await prisma.colorTheme.findMany({
       where: {
         OR: [
           { global: type === "all" || type === "global" ? true : undefined },
-          { creatorId: type === "all" || type === "my" ? req.session.userId ?? "" : undefined },
+          { creatorId: type === "all" || type === "my" ? req.session.userId ?? null : undefined },
         ],
       },
       orderBy: {
