e7d3b9d848adae61011c4a86cce1a51b50da1145
Xeeija <19950592+xeeija@users.noreply.github.com>
2026-01-23 21:10:28 +0100

    set proxy buffering in nginx config instead of next config

--- web/next.config.ts
+++ web/next.config.ts
@@ -9,15 +9,15 @@ const nextConfig: NextConfig = {
     return [
       // Disable buffering for nginx to enable streaming for app router
       // https://nextjs.org/docs/app/guides/self-hosting#streaming-and-suspense
-      {
-        source: "/:path*{/}?",
-        headers: [
-          {
-            key: "X-Accel-Buffering",
-            value: "no",
-          },
-        ],
-      },
+      // {
+      //   source: "/:path*{/}?",
+      //   headers: [
+      //     {
+      //       key: "X-Accel-Buffering",
+      //       value: "no",
+      //     },
+      //   ],
+      // },
     ]
   },
   redirects: () => [
